5041bf2ec07cdde5e9a87dde78ee2e2c8d6e770a
nabuckein <pauri@hotmail.com>
2016-12-12 15:29:22 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ $(document).ready(function(){
 
 // -- INITIALIZE VARIABLES AND CONTENT -- ////////////////////////////////////////////////////
 
-hideDynamicContent();
+//hideDynamicContent();
 $('.welcomeDiv').show(50);
+
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////
@@ -12,13 +13,17 @@ $('.welcomeDiv').show(50);
 // -- LOGIC TO SWITCH DYNAMIC CONTENT -- /////////////////////////////////////////////////////
 
 $('.iconVideo').click(function(){
-
+	
 	var icon = this.id.replace("Icon","");
 	document.getElementById('secondaryTitle').innerHTML = icon;
 	icon = "#" + icon;
-	hideDynamicContent();
+	//hideDynamicContent();
 
-	$(icon).show(100);
+	//$(icon).show(100);
+	
+	dynamicDivSelect(icon);
+	console.log(icon + "CLICKED")
+
 
 	
 });
@@ -34,4 +39,25 @@ $('.iconVideo').click(function(){
 
 function hideDynamicContent(){
 	$('.dynamicContent').hide(50);
+}
+var firsPass = 0;
+function dynamicDivSelect(div){
+	var dynamicContentDivsArray = ["#Welcome", "#Videos", "#Calendar", "#Pictures", "#Contact"];
+	
+	for (var n=0; n<=dynamicContentDivsArray.length-1; n++){
+		if(dynamicContentDivsArray[n] !== div){
+			var hiddenDiv = dynamicContentDivsArray[n];			
+			//console.log(hiddenDiv);
+			$(hiddenDiv).addClass("divDisappear");
+			$(hiddenDiv).removeClass("divAppear");
+		}else if(dynamicContentDivsArray[n] === div){
+			 
+			var shownDiv = dynamicContentDivsArray[n];
+			console.log(shownDiv);
+			$(shownDiv).removeClass("divDisappear");
+			$(shownDiv).addClass("divAppear");
+			
+		}
+	}
+	
 }
